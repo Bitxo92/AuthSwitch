a79bf3b498ce7e47c57fff4f381f2c898c0d0de3
Alejandro Patiño <alexpatino92@yahoo.com>
2026-06-27 17:42:29 +0200

    feat: documented permissions file, marking its use guidelines

--- app/frontend/src/auth/rbac/permissions.ts
+++ app/frontend/src/auth/rbac/permissions.ts
@@ -1,5 +1,16 @@
+/*
+this file  defines the permissions used for the frontend rbac. Each permission consists of a string that follows the format: <resource>-<action>. 
+The resource is the name of the resource being accessed, meaning the page(for example: "gestion_usuarios"), 
+and the action is the type of access being granted(for example: "access", "create", "update", "delete"). 
+For example, the permission "gestion_usuarios-access" grants access to the "gestion_usuarios" resource, 
+while a permission like "gestion_usuarios-create" would grant the ability to create new user in the "gestion_usuarios" resource. 
+This structure allows for fine-grained control over user access to different parts of the application. 
+*/
 export const PERMISSIONS = {
   GESTION_USUARIOS: {
     ACCESS: "gestion_usuarios-access",
+    CREATE: "gestion_usuarios-create",
+    UPDATE: "gestion_usuarios-update",
+    DELETE: "gestion_usuarios-delete",
   },
 }
